430a2128c3ccb883e27da6530cd5e9806141a121
Dongo Technology <104741434+dongoTechnology@users.noreply.github.com>
2022-07-06 21:50:27 +0800

    Angle OS V0.1.0 Beta

---
+++ dl.py
@@ -0,0 +1,18 @@
+
+
+print("="*120,end="")
+print("\b"*120,end="")
+import time
+import random
+
+for i in range(120):
+    
+    print(">"
+    ,end=""
+    ,flush = True
+    )
+    time.sleep(0.01)
+
+
+
+
